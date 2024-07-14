a5181d4ae515f8b9b20b26ff51500ea13e780f0d
vedesh-padal <vedeshpadal@gmail.com>
2024-07-14 15:20:32 +0530

    Auth middleware and some helper functions

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,6 +1,42 @@
 import fs from "fs"
 import mongoose from "mongoose"
 
+
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {string} fileName
+ * @description returns the file's static path from where the server is serving the static image
+ */
+export const getStaticFilePath = (req, fileName) => {
+  return `${req.protocol}://${req.get("host")}/images/${fileName}`;
+}
+
+/**
+ *
+ * @param {string} fileName
+ * @description returns the file's local path in the file system to assist future removal
+ */
+export const getLocalPath = (fileName) => {
+  return `public/images/${fileName}`;
+}
+
+/**
+ *
+ * @param {string} localPath
+ * @description Removed the local file from the local file system based on the file path
+ */
+export const removeLocalFile = (localPath) => {
+  fs.unlink(localPath, (err) => {
+    if (err) 
+      console.log("Error while removing local files: ", err);
+    else {
+      console.log("Remove local: ", localPath);
+    }
+  });
+};
+
+
 /**
  * @param {import("express").Request} req
  * @description **This utility function is responsible for removing unused image files due to the api fail**.
