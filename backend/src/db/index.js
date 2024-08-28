2778fbc594bc2792ec67b4b7fc17a0010d18563c
sruthin21 <125492105+sruthin21@users.noreply.github.com>
2024-08-28 18:09:59 +0530

    Added docker

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -7,7 +7,7 @@ export let dbInstance = undefined;
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}`
     );
     dbInstance = connectionInstance;
     console.log(
