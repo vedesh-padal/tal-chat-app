5d715302dc74b10e4cca575e2b540f11cb6dc107
Vedesh Padal <92447640+vedesh-padal@users.noreply.github.com>
2024-08-29 17:13:05 +0530

    Update index.js
    DB_NAME should be coming from src/constants.js
    reverted back to the previous version

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -7,7 +7,7 @@ export let dbInstance = undefined;
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     dbInstance = connectionInstance;
     console.log(
